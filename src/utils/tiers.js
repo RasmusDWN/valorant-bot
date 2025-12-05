b2f760e68b98d4d781d2d7e2a9d519ace337a25c
RasmusDWN <rasmusdyrhavenissen@gmail.com>
2025-12-05 11:50:12 +0100

    Return both a name and emote for skin tiers
    We need to host the emotes on a server and link them here.

--- src/utils/tiers.js
+++ src/utils/tiers.js
@@ -1,11 +1,23 @@
+const tierNames = {
+    "12683d76-48d7-84a3-4e09-6985794f0445": "Select",
+    "0cebb8be-46d7-c12a-d306-e9907bfc5a25": "Deluxe",
+    "60bca009-4182-7998-dee7-b8a2558dc369": "Premium",
+    "e046854e-406c-37f4-6607-19a9ba8426fc": "Exclusive",
+    "411e4a55-4e59-7757-41f0-86a53f101bb5": "Ultra"
+};
+
+const tierEmotes = {
+    "Select": "<:SelectTier:IDHERE>",
+    "Deluxe": "<:DeluxeTier:IDHERE>",
+    "Premium": "<:PremiumTier:IDHERE>",
+    "Exclusive": "<:ExclusiveTier:IDHERE>",
+    "Ultra": "<:UltraTier:IDHERE>"
+};
+
 // Fetches names of skin tiers based on their UUIDs.
 export function getTierName(uuid) {
-    const tiers = {
-        "12683d76-48d7-84a3-4e09-6985794f0445": "Select",
-        "0cebb8be-46d7-c12a-d306-e9907bfc5a25": "Deluxe",
-        "60bca009-4182-7998-dee7-b8a2558dc369": "Premium",
-        "e046854e-406c-37f4-6607-19a9ba8426fc": "Exclusive",
-        "411e4a55-4e59-7757-41f0-86a53f101bb5": "Ultra"
-    };
-    return tiers[uuid] || "Unknown";
+    const name = tierNames[uuid];
+    const emote = tierEmotes[name] || '';
+
+    return `${name} ${emote}`.trim();
 }
