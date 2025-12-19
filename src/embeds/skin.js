b045d56759c352728cc709753e55d70621567215
RasmusDWN <rasmusdyrhavenissen@gmail.com>
2025-12-19 14:16:45 +0100

    Extract skin embed

---
+++ src/embeds/skin.js
@@ -0,0 +1,27 @@
+import { EmbedBuilder } from 'discord.js';
+
+import { fetchWeaponFromSkin } from '../utils/weapons.js';
+import { getTierName, getTierPrice } from '../utils/tiers.js';
+
+export async function createSkinEmbed(skin) {
+    const chromas = skin.chromas?.length || 0;
+    const price = getTierPrice(skin.contentTierUuid);
+    const tier = skin.contentTierUuid
+        ? getTierName(skin.contentTierUuid)
+        : 'Unknown';
+    const weapon = await fetchWeaponFromSkin(skin);
+    const weaponName = weapon.displayName;
+
+    const embed = new EmbedBuilder()
+        .setTitle(weaponName)
+        .setColor('#ff4655')
+        .setThumbnail(weapon?.displayIcon || null)
+        .addFields(
+            { name: 'Price', value: price, inline: true },
+            { name: 'Tier', value: tier, inline: true },
+            { name: 'Chromas', value: chromas.toString(), inline: true }
+        )
+        .setImage(skin.fullRender || skin.displayIcon || null);
+
+    return embed;
+}
